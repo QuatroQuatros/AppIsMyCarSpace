5f867bd7227371e29e218c72949d5130dc2bf858
dandaraCarvalho <dandaracarvalhoestudos@gmail.com>
2024-03-07 21:07:09 -0300

    update tela inicial

--- app/src/main/java/br/com/fiap/buscarvagas/MainActivity.kt
+++ app/src/main/java/br/com/fiap/buscarvagas/MainActivity.kt
@@ -2,10 +2,8 @@ package br.com.fiap.buscarvagas
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ScrollView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -15,8 +13,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -36,6 +36,8 @@ import androidx.compose.ui.unit.sp
 import br.com.fiap.buscarvagas.ui.theme.BuscarVagasTheme
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,7 +60,7 @@ class MainActivity : ComponentActivity() {
 fun Main() {
     val scrollState = rememberScrollState()
 
-    Box() {
+    Box {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.verticalScroll(scrollState)
@@ -71,12 +73,12 @@ fun Main() {
                     .size(width = 500.dp, height = 60.dp)
             ) {}
             Spacer(modifier = Modifier.height(25.dp))
-            Row() {
+            Row {
                 OutlinedTextField(modifier = Modifier.size(width = 350.dp, height = 60.dp),
                     value = "",
                     onValueChange = {},
                     placeholder = {
-                        Text(text = "Régis Bittencourt, 2643")
+                        Text(text = "Pesquise por endereços")
                     }
 //                trailingIcon = {
 //                    Icon(
@@ -88,7 +90,7 @@ fun Main() {
             }
             Spacer(modifier = Modifier.height(25.dp))
 
-            Column() {
+            Column {
                 Image(
                     painter = painterResource(id = R.drawable.imagemmapa),
                     contentDescription = "imagem de um mapa",
@@ -97,13 +99,13 @@ fun Main() {
 
             }
 
-            Row() {
+            Row {
                 Text(text = "BASEADO EM SUA LOCALIZAÇÃO ATUAL:")
             }
 
             Spacer(modifier = Modifier.height(20.dp))
 
-            Row() {
+            Row {
                 Text(
                     text = "VAGAS EM",
                     color = colorResource(id = R.color.laranjaApp),
@@ -126,42 +128,40 @@ fun Main() {
 
 }
 
-@Composable()
+@Composable
 fun ParkingPlaceList() {
     ParkingPlace(
         "Shopping Taboão",
         "Régis Bittencourt, 2643",
         "shopping do taboão",
-        R.drawable.shoppingtaboao
+        R.drawable.shoppingtaboao,
+        "11 vagas disponíveis"
     )
 
 
     Spacer(modifier = Modifier.height(10.dp))
 
     ParkingPlace(
-        "Shopping Taboão",
-        "Régis Bittencourt, 2643",
+        "Drogasil",
+        "Estr. São Francisco, 2131",
         "shopping do taboão",
-        R.drawable.shoppingtaboao
+        R.drawable.drogasil,
+        "9 vagas disponíveis"
     )
 
     Spacer(modifier = Modifier.height(10.dp))
 
     ParkingPlace(
-        "Shopping Taboão",
-        "Régis Bittencourt, 2643",
-        "shopping do taboão",
-        R.drawable.shoppingtaboao
+        "Assaí Atacadista",
+        "R. João Batista, 340",
+        "mercado assaí",
+        R.drawable.assai,
+        "10 vagas disponíveis"
     )
 
     Spacer(modifier = Modifier.height(10.dp))
 
-    ParkingPlace(
-        "Shopping Taboão",
-        "Régis Bittencourt, 2643",
-        "shopping do taboão",
-        R.drawable.shoppingtaboao
-    )
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -171,6 +171,7 @@ fun ParkingPlace(
     address: String,
     contentDescription: String,
     image: Int = R.drawable.shoppingtaboao,
+    vagas: String
 ) {
     val mContext = LocalContext.current
 
@@ -195,8 +196,19 @@ fun ParkingPlace(
 
 
             Column {
-                Text(text = "$name -")
-                Text(text = address)
+                Text(text = "$name -",
+                modifier = Modifier.offset(x = 30.dp))
+                Text(text = address,
+                modifier = Modifier.offset(x = 27.dp))
+                Card(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .offset(x = 60.dp, y = 25.dp),
+                        colors = CardDefaults.cardColors(containerColor = Color(0xC35EC762))
+                ) {
+                    Text(text = vagas,
+                    modifier = Modifier.offset(x = 10.dp))
+                }
             }
         }
     }
